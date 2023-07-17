35b3aa8d7dacc4d26fc8262dfcc95a1de7e0bd1d
hidopi-tunisia <h.chebbi@hidopi.com>
2023-07-17 18:31:03 +0200

    CRUD Project

--- models/projet.js
+++ models/projet.js
@@ -1,24 +1,45 @@
 const mongoose = require("mongoose");
 
-const Projet = mongoose.model("Projet", {
-  nomProjet: {
+const ProjetSchema = new mongoose.Schema({
+  nom: {
     type: String,
+    required: true,
   },
   codeProjet: {
     type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  dateDebut: {
+    type: Date,
+    required: false,
+  },
+  dateFin: {
+    type: Date,
+    required: false,
+  },
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client",
+    required: true,
   },
   categorie: {
     type: String,
+    required: true,
   },
+  consultants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Consultant",
+  }],
   date_creation: {
     type: Date,
     default: Date.now,
-  },
-  client: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Client",
-    },
-  ],
+  }
 });
+
+const Projet = mongoose.model("Projet", ProjetSchema);
+
 module.exports = Projet;
--- routes/projet.js
+++ routes/projet.js
@@ -1,60 +1,101 @@
-const express=require('express');
+const express = require("express");
 
-const router=express.Router();
-const Projet=require('../models/projet');
-const bcrypt=require('bcrypt');
-const jwt =require('jsonwebtoken');
+const router = express.Router();
+const Projet = require("../models/projet");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.post('/create', async(req , res )=>{
+router.post("/create-projet", async (req, res) => {
+  try {
+    const {
+      nom,
+      description,
+      dateDebut,
+      dateFin,
+      client,
+      categorie,
+      codeProjet,
+    } = req.body;
+    const projet = await Projet.create({
+      nom,
+      description,
+      dateDebut,
+      dateFin,
+      client,
+      categorie,
+      codeProjet,
+    });
+    res.json(projet);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Erreur lors de la création du projet" });
+  }
+});
 
-    try{
-        data = req.body;
-        projet=new Projet(data);
-        savedProjet=await projet.save();
- 
-        res.send(savedProjet)
- 
-    } catch(error){
-        res.send(error)
-    }
- 
- })
- router.put('/update/:id',async(req,res)=>{
-    try{
-        id=req.params.id;
-        newData=req.body;
-        updated=await Projet.findByIdAndUpdate({_id:id}, newData);
-        res.send(updated);
- 
-    }catch(error){
-        res.send(err)}
-  
- })
- router.get('/getall',async(req,res)=>{
+router.put("/modifier-projet/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const {
+      nom,
+      description,
+      dateDebut,
+      dateFin,
+      client,
+      categorie,
+      codeProjet,
+    } = req.body;
+    const projet = await Projet.findByIdAndUpdate(
+      id,
+      { nom, description, dateDebut, dateFin, client, categorie, codeProjet },
+      { new: true }
+    );
+    res.json(projet);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la mise à jour du projet" });
+  }
+});
 
-    try{
- 
-        projets=await Projet.find();
-        res.send(projets);
- 
-    }catch(error){
-        res.send(error)
- 
-    }
- 
-    })
+router.get("/projet/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const projet = await Projet.findById(id).populate("client consultants");
+    res.json(projet);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Erreur lors de la récupération des détails du projet",
+    });
+  }
+});
 
-router.get('/getbyid/:id',async(req,res)=>{
-       try{
-           id=req.params.id;
-           projet=await Projet.findById({_id: id})
-           res.send(projets);
-       }catch(error){
-           res.send(error);
-       }
-   });
-   
-   // les projets d'un client précis
-   // les projets dont le status est archivé 
- module.exports=router;
- 
+router.get("/projets", async (req, res) => {
+  try {
+    const projets = await Projet.find().populate("client");
+    res.json(projets);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la récupération des projets" });
+  }
+});
+
+router.delete("/supp-projet/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Projet.findByIdAndDelete(id);
+    res.json({ message: "Projet supprimé avec succès" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la suppression du projet" });
+  }
+});
+
+// les projets d'un client précis
+// les projets dont le status est archivé
+module.exports = router;
