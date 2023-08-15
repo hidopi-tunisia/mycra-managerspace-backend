4a52fc7e2c6131f8ff84177423ebf1a660338b82
hidopi-tunisia <h.chebbi@hidopi.com>
2023-08-15 20:05:03 +0200

    afficher nbr des jrs travaillés consultant manager

--- routes/manager.js
+++ routes/manager.js
@@ -4,6 +4,7 @@ const admin = require("../config/firebaseConfig");
 const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
 const Manager = require("../models/manager");
+const Consultant= require('../models/consultant');
 
 // Configuration de Nodemailer pour utiliser un service de messagerie (par exemple, Gmail)
 const transporter = nodemailer.createTransport({
@@ -15,7 +16,38 @@ const transporter = nodemailer.createTransport({
   });
 
 //Liste des managers + nbr total
-// liste des managers actifs +nbr
+router.get("/managers", async (req, res) => {
+    try {
+      const page = parseInt(req.query.page) || 1; // Page à afficher, par défaut 1
+      const limit = parseInt(req.query.limit) || 10; // Nombre d'éléments par page, par défaut 10
+  
+      const startIndex = (page - 1) * limit;
+      const endIndex = page * limit;
+  
+      // Récupérer la liste des managers avec pagination
+      const managers = await Manager.find()
+        .skip(startIndex)
+        .limit(limit);
+  
+      // Comptage total des managers
+      const totalManagers = await Manager.countDocuments();
+  
+      // Préparer les informations de pagination
+      const pagination = {
+        currentPage: page,
+        totalPages: Math.ceil(totalManagers / limit),
+        totalItems: totalManagers,
+      };
+  
+      res.json({
+        managers,
+        pagination,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Erreur lors de la récupération des managers" });
+    }
+  });
 // liste des managers qui ont expiré la date d'essai
 // Créer un manager avec un compte
 router.post("/create-manager", async (req, res) => {
@@ -89,6 +121,89 @@ router.post("/create-manager", async (req, res) => {
   });
   exports.api = functions.https.onRequest(router);
 // modifier et archiver un manager
+router.put("/update-manager/:id", async (req, res) => {
+    try {
+      const managerId = req.params.id;
+      const updatedData = req.body;
+  
+      // Recherche et mise à jour du manager dans la base de données
+      const updatedManager = await Manager.findByIdAndUpdate(managerId, updatedData, {
+        new: true, // Renvoyer le manager mis à jour
+      });
+  
+      if (!updatedManager) {
+        return res.status(404).json({ message: "Manager non trouvé" });
+      }
+  
+      res.json({ message: "Manager mis à jour avec succès", manager: updatedManager });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Erreur lors de la mise à jour du manager" });
+    }
+  });
+  
 // Afficher un manager en détail
+router.get("/manager/:id", async (req, res) => {
+    try {
+      const managerId = req.params.id;
+  
+      // Recherche du manager dans la base de données avec les consultants associés
+      const manager = await Manager.findById(managerId).populate("consultants").exec();
+  
+      if (!manager) {
+        return res.status(404).json({ message: "Manager non trouvé" });
+      }
+  
+      const detailedManager = {
+        nom: manager.nom,
+        prenom: manager.prenom,
+        email: manager.email,
+        // ... Autres informations du manager
+        consultants: manager.consultants.map(consultant => ({
+          nom: consultant.nom,
+          prenom: consultant.prenom,
+          email: consultant.email,
+          // ... Autres informations du consultant
+        })),
+      };
+  
+      res.json({ manager: detailedManager });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Erreur lors de la récupération des informations du manager" });
+    }
+  });
+
+// Endpoint pour afficher le nombre de jours travaillés des consultants d'un manager
+router.get("/consultants/:managerId/nombre-jours-travailles", async (req, res) => {
+    try {
+        const managerId = req.params.managerId;
+
+        // Recherche du manager dans la base de données
+        const manager = await Manager.findById(managerId);
+
+        if (!manager) {
+            return res.status(404).json({ message: "Manager non trouvé" });
+        }
+
+        // Calcul du nombre total de jours travaillés par tous les consultants du manager
+        let totalDaysWorked = 0;
+
+        for (const consultantId of manager.consultants) {
+            const consultant = await Consultant.findById(consultantId);
+            if (consultant) {
+                totalDaysWorked += consultant.nombreJoursTravailles;
+            }
+        }
+
+        res.json({ 
+            message: "Nombre de jours travaillés des consultants du manager",
+            totalDaysWorked
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Erreur lors de la récupération des données" });
+    }
+});
 
 module.exports = router;
