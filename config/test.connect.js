897034a3b7a20f713b62a32c11736600114b047c
AsmaChebbi1 <chebbii.asma@gmail.com>
2023-09-26 00:01:57 +0200

    .

--- config/test.connect.js
+++ config/test.connect.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+//hello
 mongoose
   .connect("mongodb+srv://asma:as123456@cluster0.lpcnbkp.mongodb.net/mycra_test?retryWrites=true&w=majority")
   .then(async () => {
