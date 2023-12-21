210b95fb86826727e5dac8af1439d9515cf4505a
soflass1293 <soflass1293@gmail.com>
2023-12-21 14:52:08 +0100

    Update messaging.js
    Added JSON stringify data to fix the crash

--- helpers/messaging.js
+++ helpers/messaging.js
@@ -1,7 +1,7 @@
 import admin from "firebase-admin";
 
 const send = async (payload) => {
-  return admin.messaging().send(payload);
+  return admin.messaging().send(JSON.stringify(payload));
 };
 
 export { send };
